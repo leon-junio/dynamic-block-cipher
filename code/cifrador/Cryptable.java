9173585a33474ae38176f46da9b19c85fadb660e
Leon Júnio Martins <juniohuy2gamer@gmail.com>
2023-01-14 00:46:12 -0300

    Add documentation

--- code/cifrador/Cryptable.java
+++ code/cifrador/Cryptable.java
@@ -5,8 +5,23 @@ package cifrador;
  */
 public interface Cryptable {
 
+    /**
+     * Função para criptografar textos e retornar o mesmo - Function to crypt a text
+     * and return it
+     * 
+     * @param base Texto alvo para ser encriptado - Target text to be encrypted
+     * @return Texto criptografado - Crypted text
+     */
     public String crypt(String base);
 
+    /**
+     * Função para descriptografar textos e retornar o mesmo - Function to decrypt a
+     * text
+     * and return it
+     * 
+     * @param base Texto alvo para ser encriptado - Target text to be decrypted
+     * @return Texto descriptografado - Decrypted text
+     */
     public String decrypt(String base);
 
 }
